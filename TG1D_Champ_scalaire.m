e719bfa5538aa086f071182a0c77d2c04d1e0b1f
Roxane Letournel <roxane.letournel@student.ecp.fr>
2017-12-15 15:08:30 +0100

    Fonction autocorrelation

--- TG1D_Champ_scalaire.m
+++ TG1D_Champ_scalaire.m
@@ -11,7 +11,7 @@ beta=1;
 
 %Taille du maillage pour la projection
 resolution=100; 
-x=linspace(0,18,resolution+1); Nx = resolution; dx = 1/Nx;
+x=linspace(0,1,resolution+1); Nx = resolution; dx = 1/Nx;
 
 %Pas de temps
 deltat=0.01;
@@ -19,9 +19,9 @@ Fo=0.25;
 CFL=1.0;
 deltat=min([deltat,Fo*dx^2/D,CFL*dx/1 ]);
 
-Tend=100;
+Tend=0.2;
 Npas=Tend/deltat;
-N = 100;
+N = 10000;
 
 
 %Initialisation Particules Taylor Green    
@@ -38,14 +38,22 @@ U(:,1)=random('uniform',0,0,N,1);
 C = zeros(Nx,1);
 C_old = zeros(Nx,1); 
 
+figure
+plot(linspace(0,1,resolution),C)
+axis([0 1 0 0.03])
+ax = gca;
+ax.NextPlot = 'replaceChildren';
+
 for i=1:Npas-1
 
     for k=1:N
-        Ug(k,i)=sin(X(k,i));
+        Ug(k,i)=sin(2*pi*X(k,i));
     end
     
     X(:,i+1) = X(:,i)+deltat*U(:,i);
     U(:,i+1) = U(:,i)+deltat/taup*(Ug(:,i)-U(:,i));
+    
+    X(:,i+1) = mod( X(:,i+1) , 1);
     
     
     %Densité de particules
@@ -54,23 +62,6 @@ for i=1:Npas-1
     
     % champ scalaire
     C_old(:)=C(:);
-    
-%     %  First node.
-%       C(1) = C_old(1);
-%     for j=2:Nx-1
-%     %  Interior nodes.
-%           C(j) =                C_old(j) + deltat * ( ...
-%           D    * (   C_old(j+1) - 2.0 * C_old(j) + C_old(j-1)  ) / dx^2 ...
-%           - 0.5 * ( f(C_old(j+1))                - f(C_old(j-1)) ) / dx ...
-%           + alpha*density(j).^beta);
-%     end
-% 
-% %  Last node.
-%        Cr = 2.0 * C_old(Nx) - C_old(Nx-1);
-%        C(Nx) =               C_old(Nx) + deltat * ( ...
-%         D    * (   Cr - 2.0 * C_old(Nx) + C_old(Nx-1)  ) / dx^2 ...
-%         -       ( f(Cr)            - f(C_old(Nx-1)) ) / dx ...
-%         +alpha*density(Nx).^beta);
             
         for k=1:Nx           
             
@@ -80,22 +71,40 @@ for i=1:Npas-1
            %advection
               C(k) = C_old(k) - deltat*(f(C(kp1)) - f(C(k))) /dx;
 
-
            %diffusion
               C(k) = C(k) + deltat*D*(C_old(kp1) + C_old(km1) - 2*C_old(k)) /dx^2;
             %source
               C(k) = C(k) + deltat*alpha*density(k).^beta;  
             
         end
+%         plot(linspace(0,1,resolution),C,'LineWidth',2)
+        autocorr(C)
+        title(['Scalar Field for N = ',num2str(N),' particles and t =',num2str(i*deltat),' s'])
+        drawnow
+        F(i) = getframe(gcf);
     
 end
-Ug(:,Npas)=sin(X(:,Npas));
+Ug(:,Npas)=sin(2*pi*X(:,Npas));
+
+% fig = figure;
+% movie(fig,F,1)
 
 figure
+
 subplot(1,2,1)
-plot(C)
+plot(linspace(0,1,resolution),C)
+xlabel('Position')
 title(['Scalar Field for N = ',num2str(N),' particles'])
+
 subplot(1,2,2)
-scatter(X(1,:),U(1,:))
-hold on
-scatter(X(1,:),Ug(1,:))
+for k=1:10:N
+    scatter(X(k,:),U(k,:),1.5,'b','filled')
+    hold on
+end
+scatter(X(1,:),Ug(1,:),1.5,'r','filled')
+% legend('One particle','Gas')
+xlabel('Position')
+ylabel('Velocity')
+
+figure
+autocorr(C)
