d31159d82dbcb77d71108cdff57cc9ce10a3ccac
Roxane Letournel <roxane.letournel@student.ecp.fr>
2017-12-18 13:32:46 +0100

    Ns peu important

--- KLE_evolution_Ns_N.m
+++ KLE_evolution_Ns_N.m
@@ -8,25 +8,8 @@ bar(normalized_eigenvalues(1:Ns+10))
 title(['Eigenvalues for Ns =',num2str(Ns),' and Np =',num2str(N)])
 xlabel(['Sum of 3 first eigenvalues =',num2str(sum(normalized_eigenvalues(1:3)))])
 
-%% Evolution of eigenvalues for Ns fixed
-Ns = 10;
-N = [1,10,100,1000,10000,100000];
-Eigenvalues = zeros(6,100,1);
 
-for k=1:6
-    Eigenvalues(k,:,:) = KLE1D(Ns,N(k));
-end
-
-figure
-for k=1:Ns
-    plot(log10(N),Eigenvalues(:,k),'LineWidth',2)
-    hold on
-end
-xlabel('Number of particle in log scale')
-ylabel('Eigenvalues')
-title(['Evolution of eigenvalues for Ns =',num2str(Ns)])
-
-%% Evolution of eigenvalues for N fixed
+%% Evolution of eigenvalues for N fixed --> Peu important
 N = 100;
 Ns = [3,5,10,50];
 Eigenvalues = zeros(4,100,1);
@@ -43,3 +26,24 @@ end
 xlabel('Number of simulation')
 ylabel('Eigenvalues')
 title(['Evolution of eigenvalues for N =',num2str(N)])
+
+
+%% Evolution of eigenvalues for Ns fixed
+Ns = 20;
+N = [1,10,100,1000,10000,100000];
+Eigenvalues = zeros(6,100,1);
+
+for k=1:6
+    Eigenvalues(k,:,:) = KLE1D(Ns,N(k));
+end
+
+figure
+for k=1:Ns
+    plot(log10(N),Eigenvalues(:,k),'LineWidth',2)
+    hold on
+end
+xlabel('Number of particle in log scale')
+ylabel('Eigenvalues')
+title(['Evolution of eigenvalues for Ns =',num2str(Ns)])
+
+
